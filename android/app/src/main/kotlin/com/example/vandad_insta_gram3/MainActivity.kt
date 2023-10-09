ddd47640468163415af049ffb2c2226ab78824f9
Edane Barton <edanebarton@gmail.com>
2023-10-09 06:06:11 +0100

    start

---
+++ android/app/src/main/kotlin/com/example/vandad_insta_gram3/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.vandad_insta_gram3
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+//import 'package:firebase_core/firebase_core.dart';
+//import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //     //   options: DefaultFirebaseOptions.currentPlatform,
+  //     );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: '',
+      darkTheme: //ThemeData.dark(
+          ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.blueGrey,
+        indicatorColor: Colors.blueGrey,
+      ),
+      themeMode: ThemeMode.dark,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        indicatorColor: Colors.blueGrey,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              // print('Pressed');
+            },
+            style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+            child: const Text('Testing'),
+          ),
+        ],
+      ),
+    );
+  }
+}
